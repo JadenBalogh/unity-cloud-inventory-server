8a07c2d2f80b57a9e6251f7d0f89f670a810f4e5
Jaden Balogh <jadenbalogh@gmail.com>
2021-09-22 00:50:42 -0700

    Add dotenv and cleardb connection

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
 
+// dotenv setup
+dotenv.config();
+
+// express setup
 const app = express();
 const port = 3000;
+
+// database connection setup
+const conn = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
