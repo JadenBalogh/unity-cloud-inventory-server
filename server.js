7b904651214a9885bd0b48501fcf06a46d15e378
Jaden Balogh <jadenbalogh@gmail.com>
2021-10-20 22:59:55 -0700

    Add queries and fix bugs

--- server.js
+++ server.js
@@ -12,14 +12,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+let pool;
+
 // database connection setup
 function getPool() {
-  return mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE,
-  });
+  if (!pool) {
+    console.log("We didn't have a pool. Creating one!");
+    pool = mysql.createPool({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_DATABASE,
+    });
+  }
+  return pool;
 }
 
 function printResults(err, results) {
@@ -35,8 +41,35 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/get-item', (req, res) => {
+  getPool().query('SELECT * FROM item WHERE id = ?', [[req.query.itemId]], (err, results) => {
+    printResults(err, results);
+    if (err) {
+      res.send(err);
+    } else {
+      let result = results.length > 0 ? results[0] : null;
+      let output = { item: result };
+      res.send(output);
+    }
+  });
+});
+
 app.get('/get-items', (req, res) => {
-  getPool().query('SELECT * FROM item WHERE player_id = (?)', [[req.query.playerId]], (err, results) => {
+  getPool().query('SELECT * FROM item WHERE player_id = ?', [[req.query.playerId]], (err, results) => {
+    printResults(err, results);
+    if (err) {
+      res.send(err);
+    } else {
+      let output = { items: results };
+      res.send(output);
+    }
+  });
+});
+
+app.get('/get-items-by-type', (req, res) => {
+  let playerId = req.query.playerId;
+  let itemType = req.body.type;
+  getPool().query('SELECT * FROM item WHERE player_id = ? AND item_type = ?', [playerId, itemType], (err, results) => {
     printResults(err, results);
     if (err) {
       res.send(err);
@@ -50,13 +83,44 @@ app.get('/get-items', (req, res) => {
 app.post('/add-item', (req, res) => {
   let playerId = req.query.playerId;
   let itemName = req.body.name;
+  let itemType = req.body.type;
   let itemData = req.body.data;
   getPool().query(
-    'INSERT INTO item (player_id, item_name, item_data) VALUES (?, ?, ?)',
-    [playerId, itemName, itemData],
-    printResults
+    'INSERT INTO item (player_id, item_name, item_type, item_data) VALUES (?, ?, ?, ?)',
+    [playerId, itemName, itemType, itemData],
+    (err, results) => {
+      printResults(err, results);
+      if (err) {
+        res.send(err);
+      } else {
+        res.end();
+      }
+    }
   );
-  res.end();
+});
+
+app.post('/remove-item', (req, res) => {
+  getPool().query('DELETE FROM item WHERE id = ?', [req.query.itemId], (err, results) => {
+    printResults(err, results);
+    if (err) {
+      res.send(err);
+    } else {
+      res.end();
+    }
+  });
+});
+
+app.post('/trade-item', (req, res) => {
+  let itemId = req.query.itemId;
+  let playerId = req.query.playerId;
+  getPool().query('UPDATE item SET player_id = ? WHERE id = ?', [playerId, itemId], (err, results) => {
+    printResults(err, results);
+    if (err) {
+      res.send(err);
+    } else {
+      res.end();
+    }
+  });
 });
 
 app.get('/reset-items', (req, res) => {
@@ -66,6 +130,7 @@ app.get('/reset-items', (req, res) => {
       id INT AUTO_INCREMENT PRIMARY KEY,
       player_id INT NOT NULL,
       item_name VARCHAR(50) NOT NULL,
+      item_type INT NOT NULL,
       item_data TEXT
     )`,
     printResults
